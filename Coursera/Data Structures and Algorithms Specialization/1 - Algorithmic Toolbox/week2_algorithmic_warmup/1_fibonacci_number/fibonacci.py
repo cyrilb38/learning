1495e8fb2c26a6dc340aa97deb50406bdb7b7561
Cyril Benhafed <cbenhafed@cegid.com>
2022-11-24 13:34:14 +0100

    update on assignment on algo courses

--- Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,12 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
+    if n <= 1:
         return n
-
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    else :
+        fib_list = [1, 1]
+        for x in range(2, n):
+            fib_list.append(fib_list[x-1] + fib_list[x-2])
+    return fib_list[-1]
 
 n = int(input())
 print(calc_fib(n))
