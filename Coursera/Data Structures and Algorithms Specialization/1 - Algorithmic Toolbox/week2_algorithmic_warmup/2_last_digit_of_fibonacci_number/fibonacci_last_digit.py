adfa2b6679a778fe01cafef598c4c3b6becb37bf
Cyril Benhafed <cbenhafed@cegid.com>
2022-12-02 14:46:53 +0100

    2 exos on Algo assignment

--- Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -4,14 +4,11 @@ import sys
 def get_fibonacci_last_digit_naive(n):
     if n <= 1:
         return n
-
-    previous = 0
-    current  = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % 10
+    else :
+        fib_list = [1, 1]
+        for x in range(2, n):
+            fib_list.append(fib_list[x-1] % 10 + fib_list[x-2] %10)
+    return fib_list[-1] % 10
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -2,15 +2,16 @@
 import sys
 
 def gcd_naive(a, b):
-    current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
-
-    return current_gcd
+    maxounet = max([a, b])
+    minounet = min([a, b])
+    if minounet == 0:
+        return maxounet
+    else:
+        return gcd_naive(minounet, maxounet % minounet)
+    
 
 if __name__ == "__main__":
     input = sys.stdin.read()
+    # input = input()
     a, b = map(int, input.split())
     print(gcd_naive(a, b))
