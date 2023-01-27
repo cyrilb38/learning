d1cd1ef672f7c13ca8ec96a78452b8f855c4559c
Cyril Benhafed <cbenhafed@cegid.com>
2023-01-27 14:10:55 +0100

    complete week 1 assignment graph algo

--- Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
+++ Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
@@ -4,18 +4,40 @@ import sys
 
 
 def number_of_components(adj):
-    result = 0
+    count = 0
+    rest_to_process = [x for x in adj.keys()]
+    while rest_to_process:
+        count += 1
+        first_node = rest_to_process.pop(0)
+        visited = [first_node]
+        to_visit = adj[first_node]
+        while to_visit:
+            node = to_visit.pop(0)
+            visited.append(node)
+            if node in rest_to_process:
+                rest_to_process.remove(node)
+            for neighbor in adj[node]:
+                if (neighbor not in to_visit) and (neighbor not in visited):
+                    to_visit.append(neighbor)
+
     #write your code here
-    return result
+    return count
+
+# test = {
+#     "A" : ["B"],
+#     "B" : ["A"],
+#     "C" : ["D"],
+#     "D" : ["C"],
+# }
+# print(number_of_components(test))
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
+    adj = {}
+    n, m = map(int, input().split())
+    for node in range(1, n+1):
+        adj[node] = []
+    for edge in range(m):
+        a, b = map(int, input().split())
+        adj[a].append(b)
+        adj[b].append(a)
     print(number_of_components(adj))
