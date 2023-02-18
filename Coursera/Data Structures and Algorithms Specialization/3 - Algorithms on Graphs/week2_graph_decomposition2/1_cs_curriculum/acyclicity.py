4429c94e1f3f3e2ecb2531a321db3a3a88110c8e
Cyril <cyril_benhafed@hotmail.com>
2023-02-18 21:17:59 +0100

    1st ex done of week 2 assignment graph algo

--- Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week2_graph_decomposition2/1_cs_curriculum/acyclicity.py
+++ Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week2_graph_decomposition2/1_cs_curriculum/acyclicity.py
@@ -1,18 +1,41 @@
 #Uses python3
 
 import sys
+from collections import defaultdict
 
 
 def acyclic(adj):
+    visited = defaultdict(list)
+    for node in adj:
+        if node not in visited.keys():
+            to_visit = [node]
+            while to_visit:
+                current_node = to_visit.pop()
+                # print(visited, current_node)
+                for neighbor in adj[current_node]:
+                    if neighbor in visited[current_node]:
+                        # print(visited, neighbor, current_node)
+                        return 1
+                    if neighbor not in visited.keys():
+                        visited[neighbor] += visited[current_node]
+                        visited[neighbor].append(current_node)
+                        to_visit.append(neighbor)
     return 0
 
+# test = {
+#     "A" : ["B"],
+#     "B" : ["C"],
+#     "C" : ["D"],
+#     "D" : ["A"],
+# }
+# print(acyclic(test))
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
+    adj = {}
+    n, m = map(int, input().split())
+    for node in range(1, n+1):
+        adj[node] = []
+    for edge in range(m):
+        a, b = map(int, input().split())
+        adj[a].append(b)
     print(acyclic(adj))
