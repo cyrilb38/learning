eb2fbce0710416a6940261e749b2c465471ca94a
Cyril <cyril_benhafed@hotmail.com>
2023-02-24 21:18:44 +0100

    end graph aglo week 2

--- Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week2_graph_decomposition2/3_intersection_reachability/strongly_connected.py
+++ Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week2_graph_decomposition2/3_intersection_reachability/strongly_connected.py
@@ -1,22 +1,58 @@
 #Uses python3
 
 import sys
+from collections import defaultdict
 
 sys.setrecursionlimit(200000)
 
+def dfs(adj, node, time, visited):
+    #write your code here
+    time += 1
+    # print(node, visited, time)
+    visited[node]["pre"] = time
+    for neighbor in adj[node]:
+        if neighbor not in visited.keys():
+            time, visited = dfs(adj, neighbor, time, visited)
+    time+= 1
+    visited[node]["post"] = time
+    return time, visited
+
+
+def toposort(adj):
+    visited = defaultdict(dict)
+    time = 0
+    for node in adj.keys():
+        if node not in visited.keys():
+            time, visited = dfs(adj, node, time, visited)
+    #write your code here
+    return [k for k, v in sorted(visited.items(), key=lambda item: -item[1]["post"])]
+
+
+def reverse_adj(adj):
+    rev_adj = defaultdict(list)
+    for node in adj.keys():
+        for neighbor in adj[node]:
+            rev_adj[neighbor].append(node)
+    return rev_adj
 
 def number_of_strongly_connected_components(adj):
+    rev_adj = reverse_adj(adj)
     result = 0
+    visited = defaultdict(dict)
     #write your code here
+    to_process = toposort(adj)
+    for node in to_process:
+        if node not in visited.keys():
+            result += 1
+            _, visited = dfs(rev_adj, node, 0, visited)
     return result
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
+    adj = {}
+    n, m = map(int, input().split())
+    for node in range(1, n+1):
+        adj[node] = []
+    for edge in range(m):
+        a, b = map(int, input().split())
+        adj[a].append(b)
     print(number_of_strongly_connected_components(adj))
