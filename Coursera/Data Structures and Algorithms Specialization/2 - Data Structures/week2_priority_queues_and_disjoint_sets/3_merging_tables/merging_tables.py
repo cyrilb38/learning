cdd79544935205c5507ca5e189474e19b2852c2c
Cyril <cyril_benhafed@hotmail.com>
2023-04-07 16:04:36 +0200

    Complete week 2 assignement data structure

--- Coursera/Data Structures and Algorithms Specialization/2 - Data Structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
+++ Coursera/Data Structures and Algorithms Specialization/2 - Data Structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
@@ -15,15 +15,29 @@ class Database:
 
         if src_parent == dst_parent:
             return False
-
+ 
         # merge two components
         # use union by rank heuristic
         # update max_row_count with the new maximum table size
+        new_row_count = self.row_counts[src_parent] + self.row_counts[dst_parent]
+        self.row_counts[src_parent] = 0
+        self.row_counts[dst_parent] = new_row_count
+        if new_row_count > self.max_row_count:
+            self.max_row_count = new_row_count
+        
+        self.parents[src_parent] = dst_parent
+
         return True
 
     def get_parent(self, table):
         # find parent and compress path
-        return self.parents[table]
+        table_to_link = []
+        while self.parents[table] != table:
+            table_to_link.append(table)
+            table = self.parents[table]
+        for t in table_to_link:
+            self.parents[t] = table
+        return table
 
 
 def main():
