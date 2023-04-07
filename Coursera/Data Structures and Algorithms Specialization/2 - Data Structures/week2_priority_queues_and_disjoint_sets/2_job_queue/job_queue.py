4c82f29fe3305576114410528ef5d79cf1d9e255
Cyril <cyril_benhafed@hotmail.com>
2023-04-07 11:22:40 +0200

    complete ex 2 week 2 assignement data structure

--- Coursera/Data Structures and Algorithms Specialization/2 - Data Structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
+++ Coursera/Data Structures and Algorithms Specialization/2 - Data Structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
@@ -4,15 +4,62 @@ from collections import namedtuple
 
 AssignedJob = namedtuple("AssignedJob", ["worker", "started_at"])
 
+class Heap():
+    def __init__(self, array):
+        self.array = array
+        self.heapify()
+
+    def heapify(self):
+            for i in range(len(self.array)//2, -1, -1):
+                self.bubble_down(i)
+
+    def bubble_down(self, indice):
+        indice_min = None
+        if (indice * 2 + 2) < len(self.array) :
+            if (self.array[(indice * 2) + 1]["value"] < self.array[(indice * 2) + 2]["value"]):
+                indice_min = (indice * 2 + 1)
+            elif (self.array[(indice * 2) + 1]["value"] > self.array[(indice * 2) + 2]["value"]):
+                indice_min = (indice * 2 + 2)
+            elif (self.array[(indice * 2) + 1]["n_worker"] < self.array[(indice * 2) + 2]["n_worker"]):
+                indice_min = (indice * 2 + 1)
+            else :
+                indice_min = (indice * 2 + 2)
+        elif (indice * 2 + 1) < len(self.array) :
+            indice_min = (indice * 2 + 1)
+
+        if indice_min is not None and (self.array[indice_min]["value"] < self.array[indice]["value"]):     
+            self.array[indice], self.array[indice_min] = self.array[indice_min], self.array[indice]
+            self.bubble_down(indice_min)
+        elif indice_min is not None and (self.array[indice_min]["value"] == self.array[indice]["value"]) and (self.array[indice_min]["n_worker"] < self.array[indice]["n_worker"]):
+            self.array[indice], self.array[indice_min] = self.array[indice_min], self.array[indice]
+            self.bubble_down(indice_min)
+
+    def bubble_up(self, indice):
+        indice_parent = (indice - 1) // 2
+        if self.array[indice_parent]["value"] > self.array[indice]["value"]:
+            self.array[indice], self.array[indice_parent] = self.array[indice_parent], self.array[indice]
+            self.bubble_up(indice_parent)
+        elif (self.array[indice_parent]["value"] == self.array[indice]["value"]) and (self.array[indice_parent]["n_worker"] > self.array[indice]["n_worker"]):
+            self.array[indice], self.array[indice_parent] = self.array[indice_parent], self.array[indice]
+            self.bubble_up(indice_parent)
+
+    def get_min(self):
+        return self.array[0]
+    
+    def change_min_priority(self, new_value):
+        self.array[0]["value"] = new_value
+        self.bubble_down(0)
+    
 
 def assign_jobs(n_workers, jobs):
     # TODO: replace this code with a faster algorithm.
     result = []
-    next_free_time = [0] * n_workers
+    heap = Heap([{"n_worker" : x, "value" : 0} for x in range(n_workers)])
+    # print(heap.array)
     for job in jobs:
-        next_worker = min(range(n_workers), key=lambda w: next_free_time[w])
-        result.append(AssignedJob(next_worker, next_free_time[next_worker]))
-        next_free_time[next_worker] += job
+        result.append(AssignedJob(heap.get_min()["n_worker"], heap.get_min()["value"]))
+        heap.change_min_priority(heap.get_min()["value"] + job)
+        # print(heap.array)
 
     return result
 
