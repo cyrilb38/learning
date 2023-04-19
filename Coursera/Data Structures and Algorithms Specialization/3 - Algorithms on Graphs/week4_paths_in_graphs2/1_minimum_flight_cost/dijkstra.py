93d04912b3670fa8f568fb8be5980c6bcbaaa0e6
Cyril <cyril_benhafed@hotmail.com>
2023-04-19 20:10:36 +0200

    end ex 1 week 4 graph algo

--- Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.py
+++ Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.py
@@ -2,24 +2,37 @@
 
 import sys
 import queue
+import heapq
 
 
-def distance(adj, cost, s, t):
+def distance(adj, s, t):
     #write your code here
+    h = []
+    visited = set()
+    # Initialization first node
+    heapq.heappush(h, (0, s))
+    while h:
+        d, current_node = heapq.heappop(h)
+        
+        if current_node in visited:
+            continue
+        if current_node == t:
+            return d
+        
+        visited.add(current_node)
+        for edge in adj[current_node]:
+            if edge[0] not in visited:
+                heapq.heappush(h, (d + edge[1], edge[0]))
     return -1
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
-    data = data[3 * m:]
-    adj = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
-    for ((a, b), w) in edges:
-        adj[a - 1].append(b - 1)
-        cost[a - 1].append(w)
-    s, t = data[0] - 1, data[1] - 1
-    print(distance(adj, cost, s, t))
+    adj = {}
+    n, m = map(int, input().split())
+    for node in range(1, n+1):
+        adj[node] = []
+    for edge in range(m):
+        a, b, w = map(int, input().split())
+        adj[a].append((b, w))
+    s, t = map(int, input().split())
+    print(distance(adj, s, t))
