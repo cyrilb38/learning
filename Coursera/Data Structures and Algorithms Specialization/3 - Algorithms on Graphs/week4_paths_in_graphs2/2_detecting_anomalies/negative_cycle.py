d18c8f9b2bde6a4d00f8a931a0876a73281537ac
Cyril <cyril_benhafed@hotmail.com>
2023-04-24 20:28:00 +0200

    end exo 2 week 4 graph algo

--- Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
+++ Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
@@ -2,22 +2,48 @@
 
 import sys
 
+def check_negative_cycle(adj, root):
+    shortest_graph = {}
+    for node in adj.keys():
+        shortest_graph[node] = {}
+        shortest_graph[node]["d"] = 10 ** 18 # equivalent to inf
+        shortest_graph[node]["parent"] = None
+    # source
+    shortest_graph[root]["d"] = 0
+    
+    for _ in range(len(adj.keys())-1):
+        # we go through every edge to relax them if possible
+        for node in adj.keys():
+            for edge in adj[node]:
+                if (shortest_graph[edge[0]]["d"] > (shortest_graph[node]["d"] + edge[1])):
+                    shortest_graph[edge[0]]["d"] = shortest_graph[node]["d"] + edge[1]
+                    shortest_graph[edge[0]]["parent"] = node
 
-def negative_cycle(adj, cost):
-    #write your code here
+    # last iteration to detect negative cycle
+    for node in adj.keys():
+        for edge in adj[node]:
+            if (shortest_graph[edge[0]]["d"] > (shortest_graph[node]["d"] + edge[1])):
+                return 1
+
     return 0
+
+def negative_cycle(adj):
+    #write your code here
+    return check_negative_cycle(adj, list(adj.keys())[0])
+    # for node in adj.keys():
+    #     res = check_negative_cycle(adj, node)
+    #     if res == 1:
+    #         return 1
+    # return 0
+
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
-    data = data[3 * m:]
-    adj = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
-    for ((a, b), w) in edges:
-        adj[a - 1].append(b - 1)
-        cost[a - 1].append(w)
-    print(negative_cycle(adj, cost))
+    adj = {}
+    n, m = map(int, input().split())
+    for node in range(1, n+1):
+        adj[node] = []
+    for edge in range(m):
+        a, b, w = map(int, input().split())
+        adj[a].append((b, w))
+    print(negative_cycle(adj))
