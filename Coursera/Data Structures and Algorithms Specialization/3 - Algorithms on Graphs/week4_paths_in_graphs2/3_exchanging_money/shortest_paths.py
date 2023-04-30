0870144a4cfe7a35b06b601bb217dc8212182fb9
Cyril <cyril_benhafed@hotmail.com>
2023-04-30 21:07:15 +0200

    end week 4 graph algo

--- Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week4_paths_in_graphs2/3_exchanging_money/shortest_paths.py
+++ Coursera/Data Structures and Algorithms Specialization/3 - Algorithms on Graphs/week4_paths_in_graphs2/3_exchanging_money/shortest_paths.py
@@ -4,34 +4,71 @@ import sys
 import queue
 
 
-def shortet_paths(adj, cost, s, distance, reachable, shortest):
+def shortest_paths(adj, s):
     #write your code here
-    pass
+    shortest_graph = {}
+    for node in adj.keys():
+        shortest_graph[node] = {}
+        shortest_graph[node]["d"] = float("inf")
+        shortest_graph[node]["parent"] = None
+    # source
+    shortest_graph[s]["d"] = 0
+    
+    for _ in range(len(adj.keys())-1):
+        # we go through every edge to relax them if possible
+        for node in adj.keys():
+            for edge in adj[node]:
+                if (shortest_graph[edge[0]]["d"] > (shortest_graph[node]["d"] + edge[1])):
+                    shortest_graph[edge[0]]["d"] = shortest_graph[node]["d"] + edge[1]
+                    shortest_graph[edge[0]]["parent"] = node
+
+    # last iteration to detect negative cycle
+    nodes_to_check = set()
+    for node in adj.keys():
+        for edge in adj[node]:
+            if (shortest_graph[edge[0]]["d"] > (shortest_graph[node]["d"] + edge[1])) and (shortest_graph[edge[0]] != float("inf")):
+                nodes_to_check.add(edge[0])
+
+    # identify all nodes in negative cycle
+    visited = set()
+    if nodes_to_check:
+        for node in nodes_to_check:
+            # getting to the cycle
+            for _ in range(len(adj.keys())):
+                node = shortest_graph[node]["parent"]
+            first_node_in_cycle = node
+            # going upstream to get all the nodes from the cycle
+            while shortest_graph[node]["parent"] != first_node_in_cycle:
+                shortest_graph[node]["d"] = "-"
+                # all the nodes that are connected to this node needs to be tagged as such
+                to_visit = [x[0] for x in adj[node] if x[0] not in visited]
+                while to_visit:
+                    neighbor_node = to_visit.pop()
+                    visited.add(neighbor_node)
+                    shortest_graph[neighbor_node]["d"] = "-"
+                    for connected_node in adj[neighbor_node]:
+                        if (connected_node[0] not in visited) and (connected_node[0] not in to_visit):
+                            to_visit.append(connected_node[0])
+                
+                node = shortest_graph[node]["parent"] 
+
+    return shortest_graph
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
-    data = data[3 * m:]
-    adj = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
-    for ((a, b), w) in edges:
-        adj[a - 1].append(b - 1)
-        cost[a - 1].append(w)
-    s = data[0]
-    s -= 1
-    distance = [10**19] * n
-    reachable = [0] * n
-    shortest = [1] * n
-    shortet_paths(adj, cost, s, distance, reachable, shortest)
-    for x in range(n):
-        if reachable[x] == 0:
-            print('*')
-        elif shortest[x] == 0:
-            print('-')
+    adj = {}
+    n, m = map(int, input().split())
+    for node in range(1, n+1):
+        adj[node] = []
+    for edge in range(m):
+        a, b, w = map(int, input().split())
+        adj[a].append((b, w))
+    s = int(input())
+
+    res = shortest_paths(adj, s)
+    for x in range(1,len(res)+1):
+        if res[x]["d"] == float("inf"):
+            print("*")
         else:
-            print(distance[x])
+            print(res[x]["d"])
 
