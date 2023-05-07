6683d081a2ada21e63a4e4081b66395ce8010acb
Cyril <cyril_benhafed@hotmail.com>
2023-05-07 10:55:12 +0200

    complete ex 4 week 3 data structure

--- Coursera/Data Structures and Algorithms Specialization/2 - Data Structures/week3_hash_tables/4_substring_equality/substring_equality.py
+++ Coursera/Data Structures and Algorithms Specialization/2 - Data Structures/week3_hash_tables/4_substring_equality/substring_equality.py
@@ -5,8 +5,26 @@ import sys
 class Solver:
 	def __init__(self, s):
 		self.s = s
+		self.m1 = 10 ** 9 + 7
+		# self.m2 = 10 ** 9 + 9
+		self.x = 777777 # random
+		self.calculate_prefix_x()
+		self.hash_all_prefix()
+
+	def calculate_prefix_x(self):
+		self.prefix_x = [1 for _ in range(len(self.s))]
+		for i in range(1,len(self.s)):
+			self.prefix_x[i] = (self.prefix_x[i-1] * self.x) % self.m1
+
+	def hash_all_prefix(self):
+		self.all_prefix = [0 for _ in range(len(self.s))]
+		for i in range(1,len(self.s)):
+			self.all_prefix[i] = ((ord(s[i-1]) + self.all_prefix[i-1]) * self.x) % self.m1 
+
 	def ask(self, a, b, l):
-		return s[a:a+l] == s[b:b+l]
+		h_a = (self.all_prefix[a + l] - self.prefix_x[l] * self.all_prefix[a]) % self.m1
+		h_b = (self.all_prefix[b + l] - self.prefix_x[l] * self.all_prefix[b]) % self.m1
+		return h_a == h_b
 
 s = sys.stdin.readline()
 q = int(sys.stdin.readline())
