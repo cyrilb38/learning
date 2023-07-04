e7506705e30b2a82c0b705ae97ea27aa664e0687
Cyril Benhafed <cyril_benhafed@hotmail.com>
2023-07-04 17:23:49 +0200

    complete ex 3 greedy algo

--- Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -2,24 +2,34 @@
 import sys
 
 
-def compute_min_refills(distance, tank, stops):
+def compute_min_refills(distance, tank, stops, position=0, current_count=0):
     # write your code here
-    pointer = 0
-    index = 0
-    count = 0
-    while (pointer < distance) and (index < len(stops)-1):
-        while (stops[index] < pointer + tank) and (index < len(stops)-1):
-            index += 1
-        if stops[index] == pointer:
+    
+    # si le véhicule peut accéder directement au point d'arrivée, alors il n'a pas besoin de refill
+    if (position + tank) >= distance:
+        return current_count
+    else:
+        # si il n'y a pas de stops, on ne peut donc pas atteindre le bout
+        if not stops :
             return -1
-        pointer = stops[index]
-        count += 1
-    if (pointer + tank) < distance:
-        return -1
-    return count
+        # si le prochain stop est trop loin, idem
+        elif stops[0] > (position + tank):
+            return -1
+        else:
+            # Si il y a des stops, on prends le dernier que l'on peut atteindre avec notre tank
+            while stops and (stops[0] <= (position + tank)): 
+                last_stop = stops.pop(0)
+            
+            # on rappelle la fonction sur la suite du parcours
+            position = last_stop
+            current_count += 1
+            return compute_min_refills(distance, tank, stops, position, current_count)
+    
 
 # print(compute_min_refills(950, 400, [200, 375, 550, 750]))
 # print(compute_min_refills(700, 200, [100, 200, 300, 400]))
+# print(compute_min_refills(250, 250, [20, 50, 90, 140, 200]))
+# print(compute_min_refills(10, 3, [1, 2, 5, 9]))
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
