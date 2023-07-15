b32e253c3691582f79dee83eba34f8c9ca70f95d
Cyril <cyril_benhafed@hotmail.com>
2023-07-15 18:16:16 +0200

    end ex5 greedy algo

--- Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
+++ Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
@@ -1,21 +1,26 @@
 # Uses python3
 import sys
-from collections import namedtuple
-
-Segment = namedtuple('Segment', 'start end')
+import heapq
 
 def optimal_points(segments):
     points = []
     #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+    while segments:
+        _, end = heapq.heappop(segments)
+        # tant que le prochain segment ne commence pas plus loin que la fin la plus proche, on le prend
+        while segments and (end >= segments[0][0]):
+            _, current_end = heapq.heappop(segments)
+            end = min(current_end, end)
+        points.append(end)
     return points
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *data = map(int, input.split())
-    segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
+    n = int(input())
+    segments = []
+    for _ in range(n):
+        s = tuple([int(x) for x in input().split()])
+        heapq.heappush(segments, s) 
+
     points = optimal_points(segments)
     print(len(points))
     print(*points)
