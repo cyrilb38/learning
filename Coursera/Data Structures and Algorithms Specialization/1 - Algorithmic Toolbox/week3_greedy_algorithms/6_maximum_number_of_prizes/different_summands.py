5278d0c378f9b3d9852f4c90251d25627f527c07
Cyril <cyril_benhafed@hotmail.com>
2023-07-25 19:52:25 +0200

    solve ex6 week 3 algo toolbox

--- Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -3,12 +3,26 @@ import sys
 
 def optimal_summands(n):
     summands = []
-    #write your code here
+    current_count = n
+    pointer = 1
+    while current_count > 0:
+        exp_diff = current_count - pointer
+        if exp_diff == 0:
+            summands.append(pointer)
+            current_count -= pointer
+        elif (exp_diff <= pointer) :
+            pointer += 1
+        else :
+            summands.append(pointer)
+            current_count -= pointer
+            pointer += 1
+            
+        
     return summands
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
+    i = input()
+    n = int(i)
     summands = optimal_summands(n)
     print(len(summands))
     for x in summands:
