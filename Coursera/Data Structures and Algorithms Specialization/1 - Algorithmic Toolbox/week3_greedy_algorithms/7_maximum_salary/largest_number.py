710267b012f0f175ec91beaf6776e11c9e75b4aa
Cyril <cyril_benhafed@hotmail.com>
2023-08-17 20:49:17 +0200

    complete algo greedy assignment

--- Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -1,13 +1,23 @@
 #Uses python3
 
 import sys
+from functools import cmp_to_key
+
+def compare_largest_number(x, y):
+    """
+    custom compare function which look for the biggest number
+    x and y are string
+    """
+    if x == y:
+        return 0
+    elif int(x + y) > int(y + x):
+        return -1
+    else:
+        return 1
 
 def largest_number(a):
     #write your code here
-    res = ""
-    for x in a:
-        res += x
-    return res
+    return "".join(sorted(a, key=cmp_to_key(compare_largest_number)))
 
 if __name__ == '__main__':
     input = sys.stdin.read()
