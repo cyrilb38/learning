e0f2c27c6372de6d0ceb8b934fa69a7981fbc3eb
Cyril <cyril_benhafed@hotmail.com>
2023-09-21 20:30:00 +0200

    complete nb of inversion ex

--- Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week4_divide_and_conquer/5_number_of_inversions/inversions.py
+++ Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week4_divide_and_conquer/5_number_of_inversions/inversions.py
@@ -1,17 +1,34 @@
 import sys
 
-def get_number_of_inversions(a, b, left, right):
+def get_number_of_inversions(a, left, right):
     number_of_inversions = 0
     if right - left <= 1:
-        return number_of_inversions
+        return a[left:right], number_of_inversions
     ave = (left + right) // 2
-    number_of_inversions += get_number_of_inversions(a, b, left, ave)
-    number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
-    return number_of_inversions
+
+    left_part, n_inversion_left = get_number_of_inversions(a, left, ave)
+    right_part, n_inversion_right = get_number_of_inversions(a, ave, right)
+
+    number_of_inversions += n_inversion_left + n_inversion_right
+
+    ordered = []
+    while left_part or right_part:
+        if not left_part :
+            ordered += right_part
+            right_part = []
+        elif not right_part:
+            ordered += left_part
+            left_part = []
+        elif left_part[0] <= right_part[0]:
+            ordered.append(left_part.pop(0))
+        else:
+            ordered.append(right_part.pop(0))
+            number_of_inversions += len(left_part)
+
+    return ordered, number_of_inversions
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+    n = int(input())
+    a = list(map(int, input().split()))
+    _, res = get_number_of_inversions(a, 0, len(a))
+    print(res)
