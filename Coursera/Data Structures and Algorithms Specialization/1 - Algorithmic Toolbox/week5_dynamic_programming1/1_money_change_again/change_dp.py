a7ccf0b356669b1c00f7baf88ab08f2c5086d23b
Cyril <cyril_benhafed@hotmail.com>
2023-11-05 20:26:36 +0100

    complete ex 1 dynamic programming

--- Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -3,8 +3,20 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m // 4
+    COINS = (1, 3, 4)
+
+    T = {x : None for x in range(m+1)} # on prépopule le tableau
+    T[0] = 0
+    for i in range(1, max(COINS)):
+        T[-i] = float("Inf")
+
+    for i in range(1,m+1):
+        T[i] = min(
+            [T[i - coin] for coin in COINS]
+        ) + 1
+    
+    return T[m]
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
