5ae9851aaa39a5e926010ee9c5b4bac53e372558
Cyril Benhafed <cyril_benhafed@hotmail.com>
2023-11-06 12:08:25 +0100

    complete ex 2 dynamic programming

--- Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ Coursera/Data Structures and Algorithms Specialization/1 - Algorithmic Toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -2,18 +2,19 @@
 import sys
 
 def optimal_sequence(n):
-    sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
-        else:
-            n = n - 1
-    return reversed(sequence)
+    T = {i : None for i in range(1, n+1)}
+    T[1] = [1]
 
-input = sys.stdin.read()
+    for i in range(1, n+1):
+        actions = [i+1, i*2, i*3]
+        for el in actions:
+            if el in T.keys():
+                if (T[el] is None) or (len(T[el]) > (len(T[i]) + 1)):
+                    T[el] = [el] + T[i]
+
+    return reversed(T[n])
+
+input = input()
 n = int(input)
 sequence = list(optimal_sequence(n))
 print(len(sequence) - 1)
